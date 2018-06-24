a25daef971a5d5de3d530dea901633e36521713c
JackDesBwa <jackdesbwa@gmail.com>
2018-06-24 16:48:45 +0200

    Add skeletton for modes changed by keyboard

--- swv.js
+++ swv.js
@@ -63,10 +63,18 @@ function transformImage(image) {
 			console.info("StereoWebView replaces image", image);
 			image.parentNode.insertBefore(canvas, image);
 			image.parentNode.removeChild(image);
+			canvas.setAttribute("tabindex", 0);
 
 			// Expose canvas width and height to shader via u_resolution
 			var resolutionLocation = ctx.getUniformLocation(program, "u_resolution");
 			ctx.uniform2f(resolutionLocation, canvas.width, canvas.height);
+
+			// Set input and outpu modes
+			var mode = image.getAttribute("data-swv-format").charCodeAt(0);
+			var iMode = ctx.getUniformLocation(program, "u_iMode");
+			ctx.uniform1i(iMode, mode);
+			var oMode = ctx.getUniformLocation(program, "u_oMode");
+			ctx.uniform1i(oMode, mode);
 
 			// Position rectangle vertices (2 triangles)
 			var positionLocation = ctx.getAttribLocation(program, "a_position");
@@ -106,7 +114,17 @@ function transformImage(image) {
 			ctx.texImage2D(ctx.TEXTURE_2D, 0, ctx.RGBA, ctx.RGBA, ctx.UNSIGNED_BYTE, image);
 
 			// Draw the rectangle
-			ctx.drawArrays(ctx.TRIANGLES, 0, 6);
+			var redraw = function() { ctx.drawArrays(ctx.TRIANGLES, 0, 6); }
+			redraw();
+
+			canvas.addEventListener('keydown', function (event) {
+				var k = event.key;
+				if (k == "i" || k == "p" || k == "x" || k == "a" || k == "l" || k == "r") {
+					console.info("StereoWebView changes output mode to:", k);
+					ctx.uniform1i(oMode, k.charCodeAt(0));
+					redraw();
+				}
+			});
 		});
 	});
 }
--- swv_fragment.glsl
+++ swv_fragment.glsl
@@ -1,10 +1,21 @@
 precision mediump float;
 uniform sampler2D u_image; // the texture
+uniform int u_iMode; // input mode
+uniform int u_oMode; // output mode
 varying vec2 v_texCoord; // the texCoords passed from the vertex shader.
 
 void main() {
 	vec4 color = texture2D(u_image, v_texCoord);
 	float grey = (0.2126 * color.r) + (0.7152 * color.g) + (0.0722 * color.b);
-	color.rgb += (grey - color.rgb);
+	if (u_oMode == 0x70) { // p
+		color.rgb += (grey - color.rgb);
+
+	} else if (u_oMode == 0x78) { // x
+		// Do not alter pixels
+
+	} else {
+		color.rgb = vec3(0);
+	}
+
 	gl_FragColor = color;
 }
